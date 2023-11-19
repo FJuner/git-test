012a569153a3ea33e335aa4da55c0f550e5b18c6
FJun <1760686015@qq.com>
2023-11-19 22:44:03 +0800

    push commit

--- src/main/java/com/atfjun/git/GitTest.java
+++ src/main/java/com/atfjun/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello git1!!");
         System.out.println("hello git2!!!");
         System.out.println("hot-fix test!!!");
+        System.out.println("push test!");
     }
 }
