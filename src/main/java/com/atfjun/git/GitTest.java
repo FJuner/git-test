0e904f656b8544565536944fb6b64d654d56ee44
FJun <1760686015@qq.com>
2023-11-19 21:52:50 +0800

    first commit

---
+++ src/main/java/com/atfjun/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atfjun.git;
+
+/**
+ * @author FJun
+ * @ClassName GitTest
+ * @Description
+ * @date 2023/11/19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
